b52a5901d6825c19e5b0afb1637e50adf58b5fb8
Алексеева Полина <polina.mizulove@gmail.com>
2025-10-22 14:43:56 +0300

    Добавлены микросервисы для прогноза погоды: service1, service2, main_app + Docker Compose

--- simple-microservices/main_app/app.py
+++ simple-microservices/main_app/app.py
@@ -1,12 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return jsonify({"message": "Главное приложение работает!"})
+
 @app.route('/receive', methods=['POST'])
-def receive():
+def receive_message():
     data = request.json
-    print('Hello, World! Received:', data)
-    return {'status': 'ok'}
+    print(f"Main app received: {data}")
+    return jsonify({
+        "status": "success", 
+        "message": "Данные получены main_app",
+        "received_data": data
+    })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
